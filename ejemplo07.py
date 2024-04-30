cb2f4e2945e6917ab7c411e429727635ce33dfe3
Maiscal <maiscal1999@gmail.com>
2024-04-29 20:24:29 -0500

    ejemplos del 7 al 8

---
+++ ejemplo07.py
@@ -0,0 +1,12 @@
+diccionario = {}
+diccionario ['nombres'] = "Mario Isaac"
+diccionario ['apellidos'] = "Calva Abad"
+diccionario ['ciudad'] = "Loja"
+
+print(diccionario.keys())
+
+#print(diccionario["apellido"])
+#presentar todos los valores de las llaves del diccionario
+
+for x in diccionario.keys():
+	print(diccionario[x])
---
+++ ejemplo08.py
@@ -0,0 +1,5 @@
+diccionario = {'nombres': "Mario", 'apellido': "Calva", 'edad': "24"}
+diccionario ['sueldo'] = 100005.25
+
+for x in diccionario.keys():
+	print(diccionario[x])
