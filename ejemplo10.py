909f2d1b60a1b84f2d35eef60252465ef678e5c3
Maiscal <maiscal1999@gmail.com>
2024-04-29 20:54:43 -0500

    ultimos avances v2

--- ejemplo10.py
+++ ejemplo10.py
@@ -6,10 +6,10 @@ lineas = archivo.readlines()
 lineas = [l.split("|") for l in lineas]
 
 for x in lineas:
-	cadena = "Torneo: %s\nGanador: %s"%(x[0], x[9])
+	#cadena = "Torneo: %s\nGanador: %s"%(x[0], x[9])
 	cadena = """ <b>  Torneo: </b> %s<br> <b>Ganador:</b> %s"""%(x[0], x[9])
 	print(cadena)
 
-archivo_generado = open("data/%s.html" %(x[9])
+archivo_generado = open("data/%s.html" %(x[9], "w")
 archivo_generado.writelines("%s\n" % (cadena))
 archivo_generado.close()
